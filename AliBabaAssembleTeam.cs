2dd2442a5267ae044d70e97408d0a685b1905730
Mohand Mahmoud <95927128+MohandMahmoud@users.noreply.github.com>
2024-06-13 17:08:30 +0300

    Add files via upload

---
+++ AliBabaAssembleTeam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    // *****************************************
+    // DON'T CHANGE CLASS OR FUNCTION NAME
+    // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+    // *****************************************
+    public static class AliBabaAssembleTeam
+    {
+        #region YOUR CODE IS HERE
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// Find the minimum cost for any team that can be assembled 
+        /// </summary>
+        /// <param name="N">size of the array</param>
+        /// <param name="array">contains the cost of each theif (+ve, -ve or 0) </param>
+        /// <returns>min total cost of a team</returns>
+        static public long AssembleTeam(int N, short[] array)
+        {
+            //REMOVE THIS LINE BEFORE START CODING
+            throw new NotImplementedException();
+        }
+     
+        #endregion
+    }
+}
